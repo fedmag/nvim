4ffec51c855ad1423333dd67e0fcebce33f10256
fedmag <maggiorefederico@outlook.com>
2025-07-05 00:32:07 +0200

    lua line theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,26 +1,26 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	enabled = not vim.g.vscode,
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "onedark",
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch" },
-				lualine_c = {
-					{
-						"filename",
-						path = 3, -- 1 shows the relative path from the current working directory
-						shorting_target = 40, -- shorten the path if it exceeds this length
-					},
-				},
-				lualine_x = { "filetype" },
-				lualine_y = { "diff" },
-				lualine_z = { "diagnostic" },
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  enabled = not vim.g.vscode,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "kanagawa_paper",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+          {
+            "filename",
+            path = 3,       -- 1 shows the relative path from the current working directory
+            shorting_target = 40, -- shorten the path if it exceeds this length
+          },
+        },
+        lualine_x = { "filetype" },
+        lualine_y = { "diff" },
+        lualine_z = { "diagnostic" },
+      },
+    })
+  end,
 }
