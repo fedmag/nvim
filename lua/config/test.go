8f5e2727f922071c7160291e7f3ffe07dc0d4cb2
fedmag <maggiorefederico@outlook.com>
2025-07-05 16:10:09 +0200

    colors

--- lua/config/test.go
+++ lua/config/test.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// commenting 
 func main() {
 
 	fmt.Printf("test")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,7 +16,7 @@ return {
     config = function()
       require('kanagawa-paper').setup({
         color_balance = {
-          ink = { brightness = 0.1, saturation = 0.2 },
+          ink = { brightness = 0, saturation = 0 },
         },
       })
       vim.cmd.colorscheme("kanagawa-paper-ink")
