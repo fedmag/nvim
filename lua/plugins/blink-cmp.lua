fc0586c85d0b1a271aa31994c0a6595e227833ef
fedmag <maggiorefederico@outlook.com>
2025-07-11 11:53:22 +0200

    disables blink in vscode

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -3,6 +3,8 @@ return {
   -- optional: provides snippets for the snippet source
   dependencies = { 'rafamadriz/friendly-snippets' },
 
+  enabled = not vim.g.vscode,
+
   -- use a release tag to download pre-built binaries
   version = '1.*',
   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
