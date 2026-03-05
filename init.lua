eca46f73766085169c840dc78fd1123dc0c84193
fedmag <maggiorefederico@outlook.com>
2026-03-05 16:19:51 +0100

    second commit

--- init.lua
+++ init.lua
@@ -47,7 +47,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.winborder = "rounded"
 vim.opt.undofile = true
 vim.opt.clipboard:append("unnamedplus")
-vim.opt.colorcolumn = "100"                       -- show a column at 100 position chars
+vim.opt.colorcolumn = ""                          -- do not show a column at 100 position chars
 vim.opt.showmatch = true                          -- highlights matching brackets
 vim.opt.cmdheight = 1                             -- single line command line
 vim.opt.completeopt = "menuone,noinsert,noselect" -- completion options
@@ -69,32 +69,32 @@ local map = vim.keymap.set
 map('n', '<leader>so', ':update<CR> :source<CR> :lua print("Config reloaded!")<CR>')
 map('n', '<leader>e', ':lua MiniFiles.open()<CR>')
 map('n', '<leader>lf', vim.lsp.buf.format)
-vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+map("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+map("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
 
 -- other
 map('i', 'jj', '<Esc>')
 map("n", "<Esc>", ":nohlsearch<CR>", { desc = "Clear search highlights" })
 
 -- move code around
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
 -- multiple lines
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-vim.keymap.set("v", "<A-h>", "<gv", { desc = "Indent left and reselect" })
-vim.keymap.set("v", "<A-l>", ">gv", { desc = "Indent right and reselect" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+map("v", "<A-h>", "<gv", { desc = "Indent left and reselect" })
+map("v", "<A-l>", ">gv", { desc = "Indent right and reselect" })
 
 -- QOL
-vim.keymap.set("v", "p", '"_dP')
-vim.keymap.set("n", "U", "<C-r>")
-vim.keymap.set("n", "<S-h>", ":bprevious<cr>")
-vim.keymap.set("n", "<S-l>", ":bnext<cr>")
-vim.keymap.set("n", "gl", "$")
-vim.keymap.set("n", "gh", "^")
-vim.keymap.set("n", "Y", "y$")
+map("v", "p", '"_dP')
+map("n", "U", "<C-r>")
+map("n", "<S-h>", ":bprevious<cr>")
+map("n", "<S-l>", ":bnext<cr>")
+map("n", "gl", "$")
+map("n", "gh", "^")
+map("n", "Y", "y$")
 
 -- buffers
 vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
@@ -103,21 +103,27 @@ vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = 
 vim.api.nvim_set_keymap("n", "<leader>bd", ":confirm bd<CR>", { noremap = true, silent = true })
 
 -- windows
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
 -- Split window horizontally
 vim.api.nvim_set_keymap("n", "<leader>sh", ":split<CR>", { noremap = true, silent = true })
 -- Split window vertically
 vim.api.nvim_set_keymap("n", "<leader>sv", ":vsplit<CR>", { noremap = true, silent = true })
+map("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+map("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+map("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+map("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
 -- Close current window
 vim.api.nvim_set_keymap("n", "<leader>cw", ":close<CR>", { noremap = true, silent = true })
 
 -- ============================================================================
 -- PLUGIN
 -- ============================================================================
--- plugins
+
+-- ===== INTSTALL =====
 vim.pack.add({
   -- colorscheme
   { src = "https://github.com/vague2k/vague.nvim" },
@@ -125,6 +131,7 @@ vim.pack.add({
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+  { src = "https://github.com/ibhagwan/fzf-lua" },
   -- completion
   { src = "https://github.com/L3MON4D3/LuaSnip" },
   { src = "https://github.com/rafamadriz/friendly-snippets" },
@@ -133,11 +140,18 @@ vim.pack.add({
   { src = "https://github.com/nvim-mini/mini.nvim" },
   -- git
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
+  -- treesitter
+  {
+    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    build = ':TSUpdate'
+  }
 })
--- colorscheme
+
+-- ===== COLORSCHEME =====
 vim.cmd("colorscheme vague")
 
--- file explorer
+-- ===== MINI =====
 require("mini.files").setup({
   windows = {
     preview = true,
@@ -155,6 +169,7 @@ require("mini.cursorword").setup()
 require("mini.indentscope").setup()
 require("mini.notify").setup()
 require("mini.statusline").setup()
+require("mini.pick").setup()
 require("mini.tabline").setup()
 local hipatterns = require('mini.hipatterns')
 hipatterns.setup({
@@ -171,24 +186,123 @@ hipatterns.setup({
 
 })
 
--- lsp
+-- ===== LSP =====
 require("mason").setup()
 require("mason-lspconfig").setup {
-  ensure_installed = { "lua_ls", "rust_analyzer", "vtsls", "gopls", "pyright" },
+  ensure_installed = { "lua_ls", "vtsls", "gopls", "pyright" },
 }
+require("fzf-lua").setup({})
+
+local function lsp_on_attach(ev)
+  local client = vim.lsp.get_client_by_id(ev.data.client_id)
+  if not client then
+    return
+  end
+
+  local bufnr = ev.buf
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+
+  vim.keymap.set("n", "<leader>gd", function()
+    require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
+  end, opts)
+
+  vim.keymap.set("n", "<leader>gD", vim.lsp.buf.definition, opts)
+
+  vim.keymap.set("n", "<leader>gS", function()
+    vim.cmd("vsplit")
+    vim.lsp.buf.definition()
+  end, opts)
+
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+  vim.keymap.set("n", "<leader>D", function()
+    vim.diagnostic.open_float({ scope = "line" })
+  end, opts)
+  vim.keymap.set("n", "<leader>d", function()
+    vim.diagnostic.open_float({ scope = "cursor" })
+  end, opts)
+  vim.keymap.set("n", "<leader>nd", function()
+    vim.diagnostic.jump({ count = 1 })
+  end, opts)
+
+  vim.keymap.set("n", "<leader>pd", function()
+    vim.diagnostic.jump({ count = -1 })
+  end, opts)
+
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+  vim.keymap.set("n", "<leader>fd", function()
+    require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
+  end, opts)
+  vim.keymap.set("n", "<leader>fr", function()
+    require("fzf-lua").lsp_references()
+  end, opts)
+  vim.keymap.set("n", "<leader>ft", function()
+    require("fzf-lua").lsp_typedefs()
+  end, opts)
+  vim.keymap.set("n", "<leader>fs", function()
+    require("fzf-lua").lsp_document_symbols()
+  end, opts)
+  vim.keymap.set("n", "<leader>fw", function()
+    require("fzf-lua").lsp_workspace_symbols()
+  end, opts)
+  vim.keymap.set("n", "<leader>fi", function()
+    require("fzf-lua").lsp_implementations()
+  end, opts)
+
+  if client:supports_method("textDocument/codeAction", bufnr) then
+    vim.keymap.set("n", "<leader>oi", function()
+      vim.lsp.buf.code_action({
+        context = { only = { "source.organizeImports" }, diagnostics = {} },
+        apply = true,
+        bufnr = bufnr,
+      })
+      vim.defer_fn(function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end, 50)
+    end, opts)
+  end
+end
+
+
+vim.keymap.set("n", "<leader>q", function()
+  vim.diagnostic.setloclist({ open = true })
+end, { desc = "Open diagnostic list" })
+vim.keymap.set("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+
+
+
+
+
+
 -- rm global warnings
+
 vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      }
-    }
-  }
+      diagnostics = { globals = { "vim" } },
+      telemetry = { enable = false },
+    },
+  },
+})
+vim.lsp.config("pyright", {})
+vim.lsp.config("vtsls", {})
+vim.lsp.config("gopls", {})
+vim.lsp.config("clangd", {})
+
+vim.lsp.config["*"] = {
+  capabilities = require("blink.cmp").get_lsp_capabilities(),
+}
+
+vim.lsp.enable({
+  "lua_ls",
+  "pyright",
+  "vtsls",
+  "gopls",
 })
 
-
--- completion
+-- ===== COMPLETION =====
 require("luasnip.loaders.from_vscode").lazy_load()
 require("blink.cmp").setup({
   signature = { enabled = true },
@@ -213,12 +327,57 @@ require("blink.cmp").setup({
       auto_show = true,
     }
   },
-  fuzzy = { implementation = "prefer_rust_with_warning" }
+  appearance = { nerd_font_variant = "mono" },
+  sources = { default = { "lsp", "path", "buffer", "snippets" } },
+  snippets = {
+    expand = function(snippet)
+      require("luasnip").lsp_expand(snippet)
+    end,
+  },
+  fuzzy = {
+    implementation = "prefer_rust",
+    prebuilt_binaries = { download = true },
+  },
 })
 
--- git
+-- ===== GIT =====
 require('gitsigns').setup({
   current_line_blame = true
+})
+
+-- ===== TREESITTER =====
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function() vim.treesitter.start() end,
+})
+
+-- ===== DIAGNOSTIC =====
+local diagnostic_signs = {
+  Error = " ",
+  Warn = " ",
+  Hint = "",
+  Info = "",
+}
+
+vim.diagnostic.config({
+  virtual_text = { prefix = "●", spacing = 4 },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
+      [vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
+      [vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
+      [vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
+    },
+  },
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = true,
+    focusable = false,
+    style = "minimal",
+  },
 })
 -- ============================================================================
 -- AUTOMODS
@@ -232,3 +391,31 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank()
   end,
 })
+vim.api.nvim_create_autocmd("LspAttach", { group = augroup, callback = lsp_on_attach })
+
+-- this is needed so that the diagnostic window shows up if we are on that specific line
+local function au(typ, pattern, cmdOrFn)
+  if type(cmdOrFn) == 'function' then
+    vim.api.nvim_create_autocmd(typ, { pattern = pattern, callback = cmdOrFn, group = augroup })
+  else
+    vim.api.nvim_create_autocmd(typ, { pattern = pattern, command = cmdOrFn, group = augroup })
+  end
+end
+
+au({ 'CursorHold', 'InsertLeave' }, nil, function()
+  local opts = {
+    focusable = false,
+    scope = 'cursor',
+    close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter' },
+  }
+  vim.diagnostic.open_float(nil, opts)
+end)
+
+au('InsertEnter', nil, function()
+  vim.diagnostic.enable(false)
+end)
+
+au('InsertLeave', nil, function()
+  vim.diagnostic.enable(true)
+end)
+
